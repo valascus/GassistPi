c924836b2810723352f106057c105db9b7d7591a
shivasiddharth <ushivasiddharth@gmail.com>
2017-12-07 18:10:11 +0000

    Update stopbutton.py

--- src/stopbutton.py
+++ src/stopbutton.py
@@ -2,16 +2,19 @@
 import time
 import os
 import os.path
-import RPi.GPIO as GPIO
+from pyA20.gpio import gpio
+from pyA20.gpio import port
 import subprocess
 from actions import stop
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+gpio.init()
 
-while GPIO.input(23):
+#Stopbutton
+gpio.setcfg(port.PA16, gpio.INPUT)
+gpio.pullup(port.PA16, gpio.PULLUP)
+
+while gpio.input(port.PA16):
     time.sleep(0.01)
-    if not GPIO.input(23):
+    if not gpio.input(port.PA16):
        print('Stopped')
        stop() 
